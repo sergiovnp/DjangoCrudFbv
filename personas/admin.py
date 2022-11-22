062bd5752db781180500e9b89a64c8e368979324
Sergio Victor Nowosad Petruk <sergiovnp@gmail.com>
2022-11-21 22:19:42 -0300

    Se crea el Model Persona y su Admin

--- personas/admin.py
+++ personas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Persona
 
 # Register your models here.
+class PersonaAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_per_page = 9
+    search_fields = ('nombre', 'apellido')
+    list_display = ('nombre', 'apellido')
+    fields = ('nombre', 'apellido')
+    list_display_links = ('apellido',)
+
+admin.site.register(Persona, PersonaAdmin)
--- personas/models.py
+++ personas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=30, verbose_name="Nombre")
+    apellido = models.CharField(max_length=30, verbose_name="Apellido")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Editado")
+
+    class Meta:
+        verbose_name = "Personas"
+        verbose_name_plural = "Personas"
+
+    def __str__(self):
+        return self.nombre
