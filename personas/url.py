a602d321c9b7fd84538f82ed7144a20af04bc135
Sergio Victor Nowosad Petruk <sergiovnp@gmail.com>
2022-11-26 14:11:36 -0300

    Se crean view, url y template del Detalle de Persona

--- personas/url.py
+++ personas/url.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.personaLista, name='lista'),
+    path('detalle/<int:id>/', views.personaDetalle, name='detalle'),
 ]
--- personas/views.py
+++ personas/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Persona
 
 # Create your views here.
 def personaLista(request):
     personas = Persona.objects.all()
     return render (request, "lista.html", {'personas':personas} )
+
+
+def personaDetalle(request, id):
+    persona = get_object_or_404(Persona, pk=id)
+    return render(request, "detalle.html", {'persona':persona})
