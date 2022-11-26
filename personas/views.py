8cb1780b2b19e5c856203565b6ada3e16b134c9d
Sergio Victor Nowosad Petruk <sergiovnp@gmail.com>
2022-11-25 23:17:05 -0300

    Se crean templates, url, se listan registros y se configura archivos estaticos

--- personas/views.py
+++ personas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Persona
 
 # Create your views here.
+def personaLista(request):
+    personas = Persona.objects.all()
+    return render (request, "lista.html", {'personas':personas} )
